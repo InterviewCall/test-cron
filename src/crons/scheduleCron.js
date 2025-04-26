ee9a86b90e740322caafcd8dac01be79599aa06c
Arijit Ganguly <aganguly158@gmail.com>
2025-04-26 22:31:45 +0530

    slight update in cron running timing

--- src/crons/scheduleCron.js
+++ src/crons/scheduleCron.js
@@ -6,7 +6,7 @@ const { ScheduleService } = require('../services');
 
 const scheduleService = new ScheduleService(new ScheduleRepository());
 
-const job = cron.schedule('*/2 * * * *', async () => {
+const job = cron.schedule('* * * * *', async () => {
     try {
         const now = new Date();
         const todayStart = startOfToday();
