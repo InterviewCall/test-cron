38e9f8c274c137dc15ab898e1b5b8f2916985a0d
Arijit Ganguly <aganguly158@gmail.com>
2025-04-29 15:55:22 +0530

    updated timezone

--- src/crons/scheduleCron.js
+++ src/crons/scheduleCron.js
@@ -10,18 +10,18 @@ const scheduleService = new ScheduleService(new ScheduleRepository());
 const job = cron.schedule('* * * * *', async () => {
     try {
         const now = new Date();
-        const zonedTime = toZonedTime(now, 'Asia/Kolkata');
-        const todayStart = startOfToday(zonedTime);
-        const todayEnd = endOfToday(zonedTime);
+        // const zonedTime = toZonedTime(now, 'Asia/Kolkata');
+        const todayStart = startOfToday();
+        const todayEnd = endOfToday();
 
         const todayStartUtc = fromZonedTime(todayStart, 'Asia/Kolkata');  // Corrected function
         const todayEndUtc = fromZonedTime(todayEnd, 'Asia/Kolkata');
 
         console.log('start', todayStart);
         console.log('end', todayEnd);
-        console.log('now', zonedTime);
+        console.log('now', now);
 
-        await scheduleService.updateTestStatus(todayStartUtc, todayEndUtc, zonedTime);
+        await scheduleService.updateTestStatus(todayStartUtc, todayEndUtc, now);
     } catch (error) {
         console.log(error);
     }
